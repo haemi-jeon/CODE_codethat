bd90bbe8552790152ddeb4d5768c3f2968832ccf
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2023-02-12 17:16:43 +0900

    useSearchParams

--- src/pages/CourseListPage.js
+++ src/pages/CourseListPage.js
@@ -6,26 +6,25 @@ import { getCourses } from '../api';
 import styles from './CourseListPage.module.css';
 import searchBarStyles from '../components/SearchBar.module.css';
 import searchIcon from '../assets/search.svg';
+import { useSearchParams } from 'react-router-dom';
 
 function CourseListPage() {
-  const [keyword, setKeyword] = useState('');
-  const courses = getCourses();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initKeyword = searchParams.get('keyword');
+  const [keyword, setKeyword] = useState(initKeyword || '');
+  const courses = getCourses(initKeyword);
 
-  const handleKeywordChange = (e) => setKeyword(e.target.value);
+  const handleKeywordChange = e => setKeyword(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setSearchParams(keyword ? { keyword } : {});
+  };
 
   return (
-    <ListPage
-      variant="catalog"
-      title="모든 코스"
-      description="자체 제작된 코스들로 기초를 쌓으세요."
-    >
-      <form className={searchBarStyles.form}>
-        <input
-          name="keyword"
-          value={keyword}
-          onChange={handleKeywordChange}
-          placeholder="검색으로 코스 찾기"
-        ></input>
+    <ListPage variant="catalog" title="모든 코스" description="자체 제작된 코스들로 기초를 쌓으세요.">
+      <form className={searchBarStyles.form} onSubmit={handleSubmit}>
+        <input name="keyword" value={keyword} onChange={handleKeywordChange} placeholder="검색으로 코스 찾기" />
         <button type="submit">
           <img src={searchIcon} alt="검색" />
         </button>
@@ -33,15 +32,11 @@ function CourseListPage() {
 
       <p className={styles.count}>총 {courses.length}개 코스</p>
 
-      {courses.length === 0 ? (
-        <Warn
-          className={styles.emptyList}
-          title="조건에 맞는 코스가 없어요."
-          description="올바른 검색어가 맞는지 다시 한 번 확인해 주세요."
-        />
+      {initKeyword && courses.length === 0 ? (
+        <Warn className={styles.emptyList} title="조건에 맞는 코스가 없어요." description="올바른 검색어가 맞는지 다시 한 번 확인해 주세요." />
       ) : (
         <div className={styles.courseList}>
-          {courses.map((course) => (
+          {courses.map(course => (
             <CourseItem key={course.id} course={course} />
           ))}
         </div>
